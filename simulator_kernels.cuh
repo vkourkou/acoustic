54617a56d71c623c71f8ec064d37c317c7b2b66c
vkourkou <vasileios.kourkoulos@gmail.com>
2025-12-04 11:22:46 -0800

    Removing unecessary inclusion of function declarations in the header file

--- simulator_kernels.cuh
+++ simulator_kernels.cuh
@@ -7,18 +7,6 @@
 
 namespace FDTD {
 
-// CUDA kernel to update Vx field
-__global__ void
-updateVxKernel(const float* pres, float* vx, std::size_t vxRows, std::size_t vxCols, std::size_t presCols, float courantNb);
-
-// CUDA kernel to update Vy field
-__global__ void
-updateVyKernel(const float* pres, float* vy, std::size_t vyRows, std::size_t vyCols, std::size_t presCols, float courantNb);
-
-// CUDA kernel to update pressure field
-__global__ void
-updatePressureKernel(const float* vx, const float* vy, float* pres, std::size_t presRows, std::size_t presCols, std::size_t vxCols, std::size_t vyCols, float crSquareTimesCourantNb);
-
 struct CudaWorkSpace {
     CudaDenseMatrix<float> m_Pres;
     CudaDenseMatrix<float> m_Vx;
